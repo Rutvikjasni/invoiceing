b9da04454bb0057d005f51c03261108d2ee4afc8
Rutvik Jasani <ritikjasani119@gmail.com>
2023-02-17 09:29:54 +0530

    -create data and save -link click open prices page

---
+++ server.js
@@ -0,0 +1,14 @@
+// const express = require("express");
+// const app = express();
+//
+// const admin = require("firebase-admin");
+//
+//
+// app.use(express.json());
+//
+// app.use(express.urlencoded({extended: true}));
+//
+// const PORT = process.env.PORT || 8080;
+// app.listen(PORT,()=>{
+//   console.log(`server is running on PORT ${PORT}`);
+// })
